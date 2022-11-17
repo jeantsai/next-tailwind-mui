1121ec3f8e769b1f0a89fe802dadd93f0f88af8e
jeantsai <jeantsai@qq.com>
2022-11-17 16:21:07 +0800

    Add supports of both Tailwind and MUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import createCache from "@emotion/cache";
+import {CacheProvider} from "@emotion/react";
+import CssBaseline from "@mui/material/CssBaseline";
+import React from "react";
+import {StyledEngineProvider} from "@mui/material/styles";
+
+const cache = createCache({
+  key: 'css',
+  prepend: true,
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+      <CacheProvider value={cache}>
+        <StyledEngineProvider injectFirst>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </StyledEngineProvider>
+      </CacheProvider>
+  )
 }
